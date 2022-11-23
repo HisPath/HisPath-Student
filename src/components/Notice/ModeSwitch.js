60124ad2b7691d5f63d3aaa6e1c0e43e07afbbc1
lukehongg <79990740+lukehongg@users.noreply.github.com>
2022-11-23 19:06:08 +0900

    [#119]:공지 카드 적용 (#120)

--- src/components/Notice/ModeSwitch.js
+++ src/components/Notice/ModeSwitch.js
@@ -1,25 +1,49 @@
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import { Box, Typography } from '@mui/material';
+
+// table
+import TableViewIcon from '@mui/icons-material/TableView';
+import TableRowsIcon from '@mui/icons-material/TableRows';
+import FeaturedPlayListOutlinedIcon from '@mui/icons-material/FeaturedPlayListOutlined';
+
+// card
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import FontDownloadOutlinedIcon from '@mui/icons-material/FontDownloadOutlined';
+
 function ModeSwitch({ card, setCard }) {
+  const IconForSwitch = () => {
+    if (card)
+      return (
+        <>
+          <Typography variant="h5">Card&nbsp;</Typography>
+          <FontDownloadOutlinedIcon fontSize={'medium'} />
+        </>
+      );
+    else
+      return (
+        <>
+          <Typography variant="h5">Table&nbsp;</Typography>
+          <FeaturedPlayListOutlinedIcon fontSize={'medium'} />
+        </>
+      );
+  };
   return (
-    <FormGroup>
-      <FormControlLabel
-        control={
-          <Switch
-            color={card ? 'primary' : 'secondary'}
-            componentsProps={{ input: { 'aria-label': 'card mode' } }}
-            checked={card}
-            label="View Mode"
-            onChange={(event) => {
-              setCard(event.target.checked);
-            }}
-          />
-        }
-        labelPlacement="start"
-        label={`Select View Mode: ${card ? 'Card Mode' : 'Table Mode'}`}
+    <>
+      <Box container display="flex" justifyContents={'right'} paddingLeft="10px" paddingTop="6px">
+        <IconForSwitch />
+      </Box>
+      <Switch
+        color={card ? 'primary' : 'secondary'}
+        componentsProps={{ input: { 'aria-label': 'card mode' } }}
+        checked={card}
+        label="View Mode"
+        onChange={(event) => {
+          setCard(event.target.checked);
+        }}
       />
-    </FormGroup>
+    </>
   );
 }
 export default ModeSwitch;
